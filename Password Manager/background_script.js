15bbbe50242125001ea051fa2b508eda2c119006
Brbub <47784894+Brbub@users.noreply.github.com>
2019-12-02 10:44:09 -0800

    Add files via upload

---
+++ Password Manager/background_script.js
@@ -0,0 +1,10 @@
+function randomPassword(){
+var values = ["A","B","C","D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T","U","V", "W", "X", "Y", "Z","a","b","c","d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t","u","v", "w", "x", "y", "z","!","@","#","$","%","^","&","*","1","2","3","4","5","6","7","8","9","0"];
+var password="";
+    while (i < 12){
+randint = Math.random();
+password.concat(values[randint]);
+console.log(password);
+}
+}
+randomPassword();
